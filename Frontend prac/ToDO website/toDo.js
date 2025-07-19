d70a5775220ac39b3890167716efff2d60b43eb3
Sahil-Singh23 <sahil091205@gmail.com>
2025-07-20 00:12:07 +0530

    dom practise

--- Frontend prac/ToDO website/toDo.js
+++ Frontend prac/ToDO website/toDo.js
@@ -1,10 +1,25 @@
+var ctr = 1;
 function add() {
   var ele = document.querySelector("input");
   var val = ele.value;
+
   if (val) {
     var newTodo = document.createElement("div");
-    newTodo.innerHTML = val + "<button>Delete</button>";
+    newTodo.setAttribute("id", "todo-" + ctr);
+    newTodo.innerHTML =
+      ctr +
+      " " +
+      val +
+      "<button onclick='del(\"todo-" +
+      ctr +
+      "\")'>Delete</button>";
     document.getElementById("todos").appendChild(newTodo);
     ele.value = "";
+    ctr = ctr + 1;
   }
+}
+function del(ide) {
+  var ele = document.getElementById(ide);
+  ele.remove();
+  ctr--;
 }
