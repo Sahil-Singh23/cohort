4a163502d0a28d58886662dbcd40b363235b3150
Sahil-Singh23 <sahil091205@gmail.com>
2025-07-20 15:22:31 +0530

    dom practise

--- Frontend prac/ToDO website/toDo.js
+++ Frontend prac/ToDO website/toDo.js
@@ -1,24 +1,35 @@
-var ctr = 1;
-function add() {
-  var ele = document.querySelector("input");
-  var val = ele.value;
+// var ctr = 1;
+// function add() {
+//   var ele = document.querySelector("input");
+//   var val = ele.value;
 
-  if (val) {
-    var newTodo = document.createElement("div");
-    newTodo.setAttribute("id", ctr);
-    var todoText = document.createElement("span");
-    todoText.innerHTML = val;
-    var delBut = document.createElement("button");
-    delBut.innerHTML = "delete";
-    newTodo.appendChild(todoText);
-    newTodo.appendChild(delBut);
-    document.getElementById("todos").appendChild(newTodo);
-    ele.value = "";
-    ctr = ctr + 1;
-  }
+//   if (val) {
+//     var newTodo = document.createElement("div");
+//     newTodo.setAttribute("id", ctr);
+//     var todoText = document.createElement("span");
+//     todoText.innerHTML = val;
+//     var delBut = document.createElement("button");
+//     delBut.innerHTML = "delete";
+//     newTodo.appendChild(todoText);
+//     newTodo.appendChild(delBut);
+//     document.getElementById("todos").appendChild(newTodo);
+//     ele.value = "";
+//     ctr = ctr + 1;
+//   }
+// }
+// function del(ide) {
+//   var ele = document.getElementById(ide);
+//   ele.remove();
+//   ctr--;
+// }
+
+let todos = [];
+
+function addTodo() {
+  let val = document.querySelector("input").value;
+  todos.push({ title: val });
+  document.querySelector("input").value = "";
+  render();
 }
-function del(ide) {
-  var ele = document.getElementById(ide);
-  ele.remove();
-  ctr--;
-}
+
+function render() {}
