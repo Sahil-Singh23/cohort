06b1738e6f519251bb90ba9457f83d5f29cd3a5f
Sahil-Singh23 <sahil091205@gmail.com>
2025-07-28 22:07:37 +0530

    create express server

---
+++ HTTPSERV/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/add", function (req, res) {
+  const a = Number(req.query.a);
+  const b = Number(req.query.b);
+
+  res.json({ a: a, b: b, sum: a + b });
+});
+
+app.get("/sub", function (req, res) {
+  const a = Number(req.query.a);
+  const b = Number(req.query.b);
+
+  res.json({ a: a, b: b, subtract: a - b });
+});
+
+app.get("/multiply", function (req, res) {
+  const a = Number(req.query.a);
+  const b = Number(req.query.b);
+
+  res.json({ a: a, b: b, multiply: a * b });
+});
+
+app.get("/div", function (req, res) {
+  const a = Number(req.query.a);
+  const b = Number(req.query.b);
+
+  res.json({ a: a, b: b, div: a + b });
+});
+
+app.listen(3000);
