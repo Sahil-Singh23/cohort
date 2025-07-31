9990ac7804410b0b7dd9c7002e7f43f7209b0652
Sahil-Singh23 <sahil091205@gmail.com>
2025-07-31 19:26:39 +0530

    post method learning

--- HTTPSERV/index.js
+++ HTTPSERV/index.js
@@ -1,34 +1,37 @@
 const express = require("express");
+const loggerMiddlware = require("logger");
 
 const app = express();
 
-app.get("/add/:firstArg/:secondArg", function (req, res) {
-  const a = parseInt(req.params.firstArg);
-  const b = parseInt(req.params.secondArg);
+app.use(loggerMiddlware);
+
+app.post("/sum", function (req, res) {
+  const a = parseInt(req.body.a);
+  const b = parseInt(req.body.a);
 
   res.json({ a: a, b: b, sum: a + b });
 });
 
-app.get("/sub", function (req, res) {
-  const a = Number(req.query.a);
-  const b = Number(req.query.b);
+// app.get("/sub", function (req, res) {
+//   const a = Number(req.query.a);
+//   const b = Number(req.query.b);
 
-  res.json({ a: a, b: b, subtract: a - b });
-});
+//   res.json({ a: a, b: b, subtract: a - b });
+// });
 
-app.get("/multiply", function (req, res) {
-  const a = Number(req.query.a);
-  const b = Number(req.query.b);
-  ß;
+// app.get("/multiply", function (req, res) {
+//   const a = Number(req.query.a);
+//   const b = Number(req.query.b);
+//   ß;
 
-  res.json({ a: a, b: b, multiply: a * b });
-});
+//   res.json({ a: a, b: b, multiply: a * b });
+// });
 
-app.get("/div", function (req, res) {
-  const a = Number(req.query.a);
-  const b = Number(req.query.b);
+// app.get("/div", function (req, res) {
+//   const a = Number(req.query.a);
+//   const b = Number(req.query.b);
 
-  res.json({ a: a, b: b, div: a / b });
-});
+//   res.json({ a: a, b: b, div: a / b });
+// });
 
 app.listen(3000);
