d13c54b855630aada2c0ba11d692db420e08ae45
Sahil-Singh23 <sahil091205@gmail.com>
2025-08-07 20:36:46 +0530

    learned jwt s for auth requests

--- projects/auth-class/index.js
+++ projects/auth-class/index.js
@@ -1,83 +1,12 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");
 const app = express();
+const JWT_SECRET = "helloimsahilsingh";
 
 const users = [];
 
-function generateToken() {
-  let options = [
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z",
-    "A",
-    "B",
-    "C",
-    "D",
-    "E",
-    "F",
-    "G",
-    "H",
-    "I",
-    "J",
-    "K",
-    "L",
-    "M",
-    "N",
-    "O",
-    "P",
-    "Q",
-    "R",
-    "S",
-    "T",
-    "U",
-    "V",
-    "W",
-    "X",
-    "Y",
-    "Z",
-    "0",
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-  ];
-
-  let token = "";
-  for (let i = 0; i < 32; i++) {
-    // use a simple function here
-    token += options[Math.floor(Math.random() * options.length)];
-  }
-  return token;
-}
-
 app.use(express.json());
+
 app.post("/signup", (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
@@ -99,9 +28,14 @@ app.post("/signin", (req, res) => {
   );
 
   if (user) {
-    user.token = generateToken();
+    let token = jwt.sign(
+      {
+        username: user.username,
+      },
+      JWT_SECRET
+    );
     console.log(users);
-    res.json({ message: "you are successfully signed in " });
+    res.json({ message: "you are successfully signed in ", token: token });
   } else {
     res.status(403).json({ message: "Invalid user name or password" });
   }
@@ -109,8 +43,9 @@ app.post("/signin", (req, res) => {
 
 app.get("/me", (req, res) => {
   let token = req.headers.token;
+  let userName = jwt.verify(token, JWT_SECRET);
 
-  let user = users.find((u) => u.token == token);
+  let user = users.find((u) => u.username == userName);
 
   if (user) {
     res.json({ username: user.username, password: user.password });
