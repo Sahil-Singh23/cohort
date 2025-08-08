371434f0435fe892360079535b3452d8c07cdf2c
Sahil-Singh23 <sahil091205@gmail.com>
2025-08-08 10:35:50 +0530

    final jwt code

--- projects/auth-class/index.js
+++ projects/auth-class/index.js
@@ -1,57 +1,56 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
-const app = express();
-const JWT_SECRET = "helloimsahilsingh";
 
-const users = [];
+const app = express();
+const JWT_SECRET = "HELLOSAHILSINGH";
+let users = [];
 
 app.use(express.json());
-
 app.post("/signup", (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
+  const email = req.body.email;
 
   users.push({
     username: username,
     password: password,
+    email: email,
   });
-  console.log(users);
-  res.json({ message: "You are signed up" });
+
+  res.json({ message: "you are signed up" });
 });
 
 app.post("/signin", (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
 
-  let user = users.find(
+  const user = users.find(
     (u) => u.username === username && u.password === password
   );
 
   if (user) {
-    let token = jwt.sign(
-      {
-        username: user.username,
-      },
-      JWT_SECRET
-    );
-    console.log(users);
-    res.json({ message: "you are successfully signed in ", token: token });
+    const token = jwt.sign({ username: user.username }, JWT_SECRET);
+    res.json({ token: token });
   } else {
-    res.status(403).json({ message: "Invalid user name or password" });
+    res.status(401).json({ message: "invalid username or password " });
   }
 });
 
 app.get("/me", (req, res) => {
-  let token = req.headers.token;
-  let decodedToken = jwt.verify(token, JWT_SECRET);
-
-  let user = users.find((u) => u.username == decodedToken.username);
+  const token = req.headers.token;
+  let decodedToken;
+  try {
+    decodedToken = jwt.verify(token, JWT_SECRET);
+  } catch (err) {
+    return res.status(403).json({ message: "invalid token" });
+  }
+  const user = users.find((u) => u.username === decodedToken.username);
 
   if (user) {
-    res.json({ username: user.username, password: user.password });
+    res.json({ username: user.username, email: user.email });
   } else {
-    res.status(401).json({ message: "invalid token" });
+    res.status(403).json({ message: "invalid token" });
   }
 });
 
-app.listen(443);
+app.listen(3000);
