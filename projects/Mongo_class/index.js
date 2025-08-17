0dae29208a31a4efc525f3361468bb6d42455de1
Sahil-Singh23 <sahil091205@gmail.com>
2025-08-17 18:04:34 +0530

    learning mongo db

--- projects/Mongo_class/index.js
+++ projects/Mongo_class/index.js
@@ -1,29 +1,26 @@
 const express = require("express");
 const { UserModel, TodoModel } = require("./db");
-
 const app = express();
 
-app.use(express.json);
+app.use(express.json());
 
-app.post("/signup", (req, res) => {
-  const email = req.body.email;
+app.post("/signup", async (req, res) => {
   const name = req.body.name;
   const password = req.body.password;
+  const email = req.body.email;
 
-  UserModel.insert({
+  const r = await UserModel.create({
     email: email,
     password: password,
     name: name,
   });
 
-  req.json({
-    message: "you are signed up",
-  });
+  res.json({ message: "You are signed up " });
 });
 
 app.post("/signin", (req, res) => {});
 
-app.post("/todo", (req, res) => {});
+app.post("/todos", (req, res) => {});
 
 app.get("/todos", (req, res) => {});
 
