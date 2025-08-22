6bb07a97f5cdd9ddcca9be823a725a63b6173128
Sahil-Singh23 <sahil091205@gmail.com>
2025-08-22 22:40:43 +0530

    input validation using zod

--- projects/Mongo_class/index.js
+++ projects/Mongo_class/index.js
@@ -3,6 +3,7 @@ const { UserModel, TodoModel } = require("./db");
 const jwt = require("jsonwebtoken");
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const { z } = require("zod");
 require("dotenv").config();
 
 const JWT_SECRET = process.env.JWT_SECRET;
@@ -13,6 +14,25 @@ const app = express();
 app.use(express.json());
 
 app.post("/signup", async (req, res) => {
+  //input validation using zod
+  const requiredBody = z.object({
+    email: z.string().email("Email should be a valid email"),
+    name: z.string().min(2).max(50),
+    password: z
+      .string()
+      .min(6, "Password must have atleast 6 charracters")
+      .max(100, "Password can not have more than 100 characters")
+      .regex(/[A-Z]/, "Password must contain atleast one uppercase character")
+      .regex(/[a-z]/, "Password must contain atleast one lowercase character")
+      .regex(/^[A-za-z0-9]/, "Must contain atleast one special character"),
+  });
+
+  const parsedData = requiredBody.safeParse(req.body);
+
+  if (!parsedData.success) {
+    res.json({ message: "Incorrect format", error: parsedData.error });
+    return;
+  }
   const name = req.body.name;
   const password = req.body.password;
   const email = req.body.email;
