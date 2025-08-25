50292ae932a589c66a200306c2d3892b11880bb9
Sahil-Singh23 <sahil091205@gmail.com>
2025-08-26 02:45:25 +0530

    created a fully functional signin endpoint

--- projects/course-selling-app/routes/admin.js
+++ projects/course-selling-app/routes/admin.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const adminRouter = express.Router();
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const { z, email } = require("zod");
 const { adminModel } = require("../db");
 
@@ -19,12 +20,24 @@ adminRouter.post("/signup", async (req, res) => {
   });
 
   const safeParsedBody = requiredBody.safeParse(req.body);
-
+  // returnes the entire error
   if (!safeParsedBody.success) {
     return res
       .status(400)
       .json({ message: "Incorrect format", error: safeParsedBody.error });
   }
+  // returnes the only error
+  // if (!safeParsedBody.success) {
+  //   const formattedErrors = safeParsedBody.error.errors.map((err) => ({
+  //     path: err.path.join("."),
+  //     message: err.message,
+  //   }));
+
+  //   return res.status(400).json({
+  //     message: "Validation failed",
+  //     errors: formattedErrors,
+  //   });
+  // }
 
   const { email, password, fname, lname } = safeParsedBody.data;
 
@@ -39,11 +52,53 @@ adminRouter.post("/signup", async (req, res) => {
 
     res.json({ message: "You are signed up sucessfullly " });
   } catch (e) {
-    res.status(409).json({ message: "User already exists" });
+    res.status(409).json({ message: "Admin email already exists" });
   }
 });
 
-adminRouter.post("/signin", (req, res) => {});
+adminRouter.post("/signin", async (req, res) => {
+  const requiredBody = z.object({
+    email: z.string().email("Email should be a valid email"),
+    password: z
+      .string()
+      .min(6, "Password must have atleast 6 characters")
+      .max(100, "Password can not have more than 100 characters")
+      .regex(/[a-z]/, "Password must contain atleast one lowercase character")
+      .regex(/[A-Z]/, "Password must contain atleast one uppercase character")
+      .regex(/[^ a-zA-Z0-9]/, "Must contain atleast one special character"),
+  });
+
+  const safeParsedBody = requiredBody.safeParse(req.body);
+
+  // returnes the entire error
+  if (!safeParsedBody.success) {
+    return res
+      .status(400)
+      .json({ message: "Incorrect format", error: safeParsedBody.error });
+  }
+
+  const { password, email } = safeParsedBody.data;
+
+  const user = await adminModel.findOne({
+    email,
+  });
+
+  if (user) {
+    const match = await bcrypt.compare(password, user.password);
+    if (match) {
+      const token = jwt.sign(
+        { userId: user._id },
+        process.env.JWT_ADMIN_SECRET,
+        { expiresIn: "6d" }
+      );
+      return res.status(200).json({ message: "Admin is signed in", token });
+    } else {
+      return res.status(403).json({ message: "Password is incorrect" });
+    }
+  } else {
+    return res.status(403).json({ message: "Invalid email" });
+  }
+});
 
 adminRouter.post("/course", (req, res) => {});
 
--- projects/course-selling-app/routes/users.js
+++ projects/course-selling-app/routes/users.js
@@ -1,21 +1,62 @@
 const express = require("express");
 const userRouter = express.Router();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { z, email } = require("zod");
+const { userModel } = require("../db");
 
-userRouter.post("/signup", (req, res) => {});
+userRouter.post("/signup", async (req, res) => {
+  const requiredBody = z.object({
+    email: z.string().email("Email should be a valid email"),
+    lname: z.string().min(2).max(100),
+    fname: z.string().min(2).max(100),
+    password: z
+      .string()
+      .min(6, "Password must have atleast 6 characters")
+      .max(100, "Password can not have more than 100 characters")
+      .regex(/[a-z]/, "Password must contain atleast one lowercase character")
+      .regex(/[A-Z]/, "Password must contain atleast one uppercase character")
+      .regex(/[^ a-zA-Z0-9]/, "Must contain atleast one special character"),
+  });
+
+  const safeParsedBody = requiredBody.safeParse(req.body);
+  // returnes the entire error
+  if (!safeParsedBody.success) {
+    return res
+      .status(400)
+      .json({ message: "Incorrect format", error: safeParsedBody.error });
+  }
+
+  const { email, password, fname, lname } = safeParsedBody.data;
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 5);
+    const dbResult = await userModel.create({
+      email: email,
+      password: hashedPassword,
+      firstName: fname,
+      lastName: lname,
+    });
+
+    res.json({ message: "You are signed up sucessfullly " });
+  } catch (e) {
+    res.status(409).json({ message: "User email already exists" });
+  }
+});
 
 userRouter.post("/signin", (req, res) => {});
 
-const auth = async (req, res, next) => {
-  const token = req.headers.token;
+// const auth = async (req, res, next) => {
+//   const token = req.headers.token;
 
-  const decodedToken = jwt.verify(token, jwt_secret);
-  if (decodedToken) {
-    req.userId = decodedToken.userId;
-    next();
-  } else {
-    res.status(403).json({ message: "Invalid or expired token " });
-  }
-};
+//   const decodedToken = jwt.verify(token, jwt_secret);
+//   if (decodedToken) {
+//     req.userId = decodedToken.userId;
+//     next();
+//   } else {
+//     res.status(403).json({ message: "Invalid or expired token " });
+//   }
+// };
 
 userRouter.get("/purchases", (req, res) => {});
 
