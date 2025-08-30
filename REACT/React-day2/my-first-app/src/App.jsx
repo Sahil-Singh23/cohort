576df8b0df80dd022a46d36dee71ad9a54b2fee3
Sahil-Singh23 <sahil091205@gmail.com>
2025-08-30 23:39:59 +0530

    learned useState

--- REACT/React-day2/my-first-app/src/App.jsx
+++ REACT/React-day2/my-first-app/src/App.jsx
@@ -1,5 +1,33 @@
+import { useState } from "react";
+
 function App() {
-  return <> </>;
+  return (
+    <>
+      <Counter></Counter>
+    </>
+  );
+}
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  function increaseCount() {
+    setCount(count + 1);
+  }
+  function decreaseCount() {
+    setCount(count - 1);
+  }
+  function resetCount() {
+    setCount(0);
+  }
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={increaseCount}>Increase Count</button>
+      <button onClick={decreaseCount}>Decrease Count</button>
+      <button onClick={resetCount}>Reset Count</button>
+    </div>
+  );
 }
 
 export default App;
