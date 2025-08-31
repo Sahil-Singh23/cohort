bc7af8d2c4bab8ff3dcd7fb6b59e4d00c93f31d3
Sahil-Singh23 <sahil091205@gmail.com>
2025-08-31 22:47:13 +0530

    learned useState

--- REACT/React-day2/my-first-app/src/App.jsx
+++ REACT/React-day2/my-first-app/src/App.jsx
@@ -11,21 +11,14 @@ function App() {
 function Counter() {
   const [count, setCount] = useState(0);
 
-  function increaseCount() {
+  setTimeout(() => {
     setCount(count + 1);
-  }
-  function decreaseCount() {
-    setCount(count - 1);
-  }
-  function resetCount() {
-    setCount(0);
-  }
+  }, 1000);
+  console.log("counter");
+
   return (
     <div>
       <h1>{count}</h1>
-      <button onClick={increaseCount}>Increase Count</button>
-      <button onClick={decreaseCount}>Decrease Count</button>
-      <button onClick={resetCount}>Reset Count</button>
     </div>
   );
 }
