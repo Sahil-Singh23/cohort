bdf234c988be46644a78a066c1fafe6124b9d0b6
Sahil-Singh23 <sahil091205@gmail.com>
2025-09-03 20:43:31 +0530

    learned about props in react , and halfway through the tic tac to

--- REACT/React-day2/my-first-app/src/App.jsx
+++ REACT/React-day2/my-first-app/src/App.jsx
@@ -1,45 +1,30 @@
-import { useEffect, useState } from "react";
-import { set } from "zod";
+import { use, useEffect, useState } from "react";
 
+//Re - learning cleanup, useEffect, learning about dependancy array
 function App() {
-  let [counterVisible, setCounterVisible] = useState(true);
-
-  useEffect(() => {
-    setInterval(() => {
-      setCounterVisible((c) => !c);
-    }, 5000);
-  }, []);
-  return <>{counterVisible ? <Counter></Counter> : null} </>;
-}
-//mounting, re-rendering, unmounting
-//for mounting we use the useEffect hook ,
-//lifecycle events
-function Counter() {
   const [count, setCount] = useState(0);
-  console.log("counter");
-
-  //guard our intervals from re renders
-  //is used to mount something to the component
-  useEffect(() => {
-    setInterval(() => {
-      setCount((count) => count + 1);
-    }, 1000);
-    console.log("mounted");
-  }, []); // dependacny array
+  function increaseCount() {
+    setCount(count + 1);
+  }
 
   return (
     <div>
-      <h1>{count}</h1>
+      <Counter count={count}></Counter>
+      <button onClick={increaseCount}>Click</button>
     </div>
   );
 }
 
-export default App;
+function Counter(props) {
+  useEffect(() => {
+    console.log("Mount");
 
-// use state
-// hooks - they hook to life cycle, like use state or use effect
-//use effect
-//cleanup
-//using fetch inside hook
-//conditonal rendering
-//hot module replacement hmr
+    return () => {
+      console.log("Unmount");
+    };
+  }, []);
+
+  return <div>Counter {props.count}</div>;
+}
+
+export default App;
--- REACT/first-react-app/src/App.jsx
+++ REACT/first-react-app/src/App.jsx
@@ -4,20 +4,39 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [todos, setTodo] = useState([
-    {
-      title: "hit the gym",
-      done: false,
-    },
-  ]);
+  const [state, setState] = useState(Array(9).fill(null));
 
-  function addTodo() {
-    setCount(count + 1);
-    setTodo(todos.push);
+  function onSquareClick(i) {
+    const newArr = state.slice();
+    newArr[i] = "x";
+    setState(newArr);
   }
   return (
     <>
-      <button onClick={addTodo}>Counter {count}</button>
+      <div className="board-row">
+        <Square count={state[0]} onClick={() => onSquareClick(0)}></Square>
+        <Square count={state[1]} onClick={() => onSquareClick(1)}></Square>
+        <Square count={state[2]} onClick={() => onSquareClick(2)}></Square>
+      </div>
+      <div className="board-row">
+        <Square count={state[3]} onClick={() => onSquareClick(3)}></Square>
+        <Square count={state[4]} onClick={() => onSquareClick(4)}></Square>
+        <Square count={state[5]} onClick={() => onSquareClick(5)}></Square>
+      </div>
+      <div className="board-row">
+        <Square count={state[6]} onClick={() => onSquareClick(6)}></Square>
+        <Square count={state[7]} onClick={() => onSquareClick(7)}></Square>
+        <Square count={state[8]} onClick={() => onSquareClick(8)}></Square>
+      </div>
+    </>
+  );
+}
+function Square(props) {
+  return (
+    <>
+      <button className="button" onClick={props.onClick}>
+        {props.count}
+      </button>
     </>
   );
 }
