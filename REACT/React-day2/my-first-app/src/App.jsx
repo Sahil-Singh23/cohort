b756f82f717831dd5fec780fc30adc00cdf1a3cc
Sahil-Singh23 <sahil091205@gmail.com>
2025-09-23 18:54:12 +0530

    learning use effect hook

--- REACT/React-day2/my-first-app/src/App.jsx
+++ REACT/React-day2/my-first-app/src/App.jsx
@@ -3,28 +3,36 @@ import { use, useEffect, useState } from "react";
 //Re - learning cleanup, useEffect, learning about dependancy array
 function App() {
   const [count, setCount] = useState(0);
-  function increaseCount() {
-    setCount(count + 1);
+  const [count2, setCount2] = useState(0);
+
+  function increase() {
+    setCount((count) => count + 1);
+  }
+  function decrease() {
+    setCount2((count) => count - 1);
   }
 
   return (
-    <div>
-      <Counter count={count}></Counter>
-      <button onClick={increaseCount}>Click</button>
-    </div>
+    <>
+      <Counter count={count} count2={count2}></Counter>
+      <div>
+        <button onClick={increase}>Increase</button>
+        <button onClick={decrease}>Decrease</button>
+      </div>
+    </>
   );
 }
 
-function Counter(props) {
+function Counter({ count, count2 }) {
   useEffect(() => {
-    console.log("Mount");
-
-    return () => {
-      console.log("Unmount");
-    };
-  }, []);
-
-  return <div>Counter {props.count}</div>;
+    console.log("count has changed");
+  }, [count, count2]);
+  return (
+    <>
+      Counter {count}
+      Counter {count2}
+    </>
+  );
 }
 
 export default App;
