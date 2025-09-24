ede7aa1f086d99649d981a89ccf043796da6e586
Sahil-Singh23 <sahil091205@gmail.com>
2025-09-24 22:09:30 +0530

    learned how to crete single page application or routing in react

--- REACT/React-routing/src/App.jsx
+++ REACT/React-routing/src/App.jsx
@@ -1,15 +1,25 @@
 import { useState } from "react";
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+  useNavigate,
+} from "react-router-dom";
+import { useEffect } from "react";
 
 function App() {
   return (
     <>
-      <div>TOP BAR</div>
       <BrowserRouter>
+        <div>
+          <Link to="/">Main </Link> |<Link to="/home"> Home</Link>
+        </div>
         <Routes>
           <Route path="/" element={<Main></Main>}></Route>
           <Route path="/home" element={<Home></Home>}></Route>
+          <Route path="*" element={<ErrorPage> </ErrorPage>}></Route>
         </Routes>
       </BrowserRouter>
     </>
@@ -21,6 +31,12 @@ function Home() {
 }
 
 function Main() {
+  const nav = useNavigate();
+  useEffect(() => {
+    setTimeout(() => {
+      nav("/home");
+    }, 3000);
+  }, []);
   return <>Welcome to my website !</>;
 }
 
