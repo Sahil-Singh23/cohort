c6d84d3e3eee852ee38b42b6409088ae2da34760
Sahil-Singh23 <sahil091205@gmail.com>
2025-09-28 01:23:11 +0530

    3 use cases of use ref

--- REACT/React-routing/src/App.jsx
+++ REACT/React-routing/src/App.jsx
@@ -1,29 +1,2 @@
 import { useState } from "react";
 import "./App.css";
-import { useRef } from "react";
-import { set } from "zod";
-
-function App() {
-  const [cur, setCur] = useState(0);
-  const timer = useRef();
-
-  function startClock() {
-    timer.current = setInterval(() => {
-      setCur((c) => c + 1);
-    }, 1000);
-  }
-
-  function stopClock() {
-    clearInterval(timer.current);
-  }
-
-  return (
-    <>
-      <div>{cur}</div>
-      <button onClick={startClock}>Start</button>
-      <button onClick={stopClock}>Stop</button>
-    </>
-  );
-}
-
-export default App;
