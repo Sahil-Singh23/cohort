3cf12f848d0034709bcf69a6d505f8acc79ce1af
Sahil-Singh23 <sahil091205@gmail.com>
2025-09-30 02:04:34 +0530

    created useFetch hook improved

--- REACT/customHooks/src/App.jsx
+++ REACT/customHooks/src/App.jsx
@@ -4,9 +4,20 @@ import { useEffect } from "react";
 import { useFetch } from "./hooks/useFetch";
 
 function App() {
-  const { post } = useFetch("https://jsonplaceholder.typicode.com/todos/5");
+  const [curentPost, setCurrentPost] = useState(1);
+  const { post, loading } = useFetch(
+    "https://jsonplaceholder.typicode.com/todos/" + curentPost
+  );
 
-  return <div>{post.title}</div>;
+  return (
+    <div>
+      <button onClick={() => setCurrentPost(1)}>1st post</button>
+      <button onClick={() => setCurrentPost(2)}>2nd post</button>
+      <button onClick={() => setCurrentPost(3)}>3rd post</button>
+      <button onClick={() => setCurrentPost(4)}>4th post</button>
+      <div>{loading ? "loading..." : JSON.stringify(post)} </div>
+    </div>
+  );
 }
 
 export default App;
--- REACT/customHooks/src/hooks/useFetch.js
+++ REACT/customHooks/src/hooks/useFetch.js
@@ -2,16 +2,19 @@ import { useState, useEffect } from "react";
 
 export function useFetch(url) {
   const [post, setPost] = useState({});
+  const [loading, setLoading] = useState(false);
 
   async function getPosts() {
+    setLoading(true);
     const res = await fetch(url);
     const json = await res.json();
 
     setPost(json);
+    setLoading(false);
   }
   useEffect(() => {
     getPosts();
-  }, []);
+  }, [url]);
 
-  return { post };
+  return { post, loading };
 }
