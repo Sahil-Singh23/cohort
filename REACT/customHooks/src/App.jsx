6fca7bc973d52400c880831c9aee90a923cb4fd1
Sahil-Singh23 <sahil091205@gmail.com>
2025-09-30 21:31:05 +0530

    created usePrev hook

--- REACT/customHooks/src/App.jsx
+++ REACT/customHooks/src/App.jsx
@@ -2,20 +2,16 @@ import { useState } from "react";
 import "./App.css";
 import { useEffect } from "react";
 import { useFetch } from "./hooks/useFetch";
+import { usePrev } from "./hooks/usePrev";
 
 function App() {
-  const [curentPost, setCurrentPost] = useState(1);
-  const { post, loading } = useFetch(
-    "https://jsonplaceholder.typicode.com/todos/" + curentPost
-  );
+  const [state, setState] = useState(0);
+  const prev = usePrev(state);
 
   return (
     <div>
-      <button onClick={() => setCurrentPost(1)}>1st post</button>
-      <button onClick={() => setCurrentPost(2)}>2nd post</button>
-      <button onClick={() => setCurrentPost(3)}>3rd post</button>
-      <button onClick={() => setCurrentPost(4)}>4th post</button>
-      <div>{loading ? "loading..." : JSON.stringify(post)} </div>
+      <button onClick={() => setState((c) => c + 1)}>Increase{state}</button>
+      <div>{prev}</div>
     </div>
   );
 }
