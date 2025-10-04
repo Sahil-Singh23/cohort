38f9774652dea9946b2d0ab7d0769e99f8ffeaf2
Sahil-Singh23 <sahil091205@gmail.com>
2025-10-04 19:45:30 +0530

    done with custom hooks

--- REACT/customHooks/src/App.jsx
+++ REACT/customHooks/src/App.jsx
@@ -3,19 +3,37 @@ import "./App.css";
 import { useEffect } from "react";
 import { useFetch } from "./hooks/useFetch";
 import { usePrev } from "./hooks/usePrev";
-import { useDebounce } from "./hooks/useDebouce";
+//import { useDebounce } from "./hooks/useDebouce";
+
+const useDebounce = (value, delay) => {
+  const [debouncedVal, setDebouncedVal] = useState(value);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setDebouncedVal(value), delay);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [value, delay]);
+
+  return debouncedVal;
+};
 
 function App() {
-  function sendDataToBackend() {
-    console.log("req sent to backend");
-    //fetch("xyz.com/search");
-  }
+  const [inputVal, setInputVal] = useState("");
+  const debouncedVal = useDebounce(inputVal, 200);
 
-  const debouncedFn = useDebounce(sendDataToBackend);
+  const change = (e) => {
+    setInputVal(e.target.value);
+    console.log("val changed");
+  };
+
+  useEffect(() => {
+    console.log("Expensive operation performed");
+  }, [debouncedVal]);
 
   return (
     <div>
-      <input type="text" onChange={debouncedFn}></input>
+      <input type="text" value={inputVal} onChange={change}></input>
     </div>
   );
 }
