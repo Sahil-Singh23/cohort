0ba3231f4b311ee7b87be46d85c3118ed046cc90
Sahil-Singh23 <sahil091205@gmail.com>
2025-10-13 02:26:25 +0530

    learning tailwind

---
+++ REACT/Tailwind class/tailwind/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="flex justify-around">
+        <div>
+          <div className="bg-blue-200">hello</div>
+        </div>
+        <div className="flex items-center gap-4">
+          <div className="bg-blue-200">hello</div>
+          <div className="bg-blue-200">hello</div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
