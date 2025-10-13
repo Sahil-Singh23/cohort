c53b19755e17d8924be42835752598ec065fc74b
Sahil-Singh23 <sahil091205@gmail.com>
2025-10-13 22:00:33 +0530

    learned grids n flex in tailwind

--- REACT/Tailwind class/tailwind/src/App.jsx
+++ REACT/Tailwind class/tailwind/src/App.jsx
@@ -3,10 +3,10 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div className="flex flex-col sm:flex-row sm:justify-around ">
-        <div className="bg-[#967969] w-40%">hello</div>
-        <div className="bg-red-200 w-40%">hello</div>
-        <div className="bg-green-200 w-20%">hello</div>
+      <div className="grid grid-cols-12">
+        <div className="bg-[#967969] col-span-12 sm:col-span-4">hello</div>
+        <div className="bg-red-200 col-span-12 sm:col-span-4">hello</div>
+        <div className="bg-green-200 col-span-12 sm:col-span-4">hello</div>
       </div>
     </>
   );
