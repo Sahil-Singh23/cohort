b6d884fd2a3640896f0f41027b08e6c3af68b2d1
Sahil-Singh23 <sahil091205@gmail.com>
2025-12-26 20:25:45 +0530

    interface in ts

--- TypeScript/index.ts
+++ TypeScript/index.ts
@@ -1,18 +1,14 @@
-
-
-function delayedCall(fn : ()=> void){
-    setTimeout(fn,2000);
-}
-delayedCall(()=>{
-    console.log('Hello after 2 seconds');
-})
-
 //object as input
-
-function greet(user: {name:String,age:number}) :void{
+interface userType{
+    name:String,
+    age:number
+}
+function greet(user: userType) :void{
     console.log(user.name);
     console.log(user.age);
 }
-greet({name:'Sahil',
+let user:userType = {
+    name:'Sahil',
     age:20
-})
+}
+greet(user);
