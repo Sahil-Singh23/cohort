1cab71bd460630eba960a7591f81617ae00fc9e3
Sahil-Singh23 <sahil091205@gmail.com>
2025-12-26 19:28:25 +0530

    learning intefaces in ts

--- TypeScript/index.ts
+++ TypeScript/index.ts
@@ -6,3 +6,13 @@ function delayedCall(fn : ()=> void){
 delayedCall(()=>{
     console.log('Hello after 2 seconds');
 })
+
+//object as input
+
+function greet(user: {name:String,age:number}) :void{
+    console.log(user.name);
+    console.log(user.age);
+}
+greet({name:'Sahil',
+    age:20
+})
