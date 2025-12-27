bf48a3aeb9b4f17381c67930202de6fc68bd59aa
Sahil-Singh23 <sahil091205@gmail.com>
2025-12-27 23:13:44 +0530

    interface in ts

--- TypeScript/index.js
+++ TypeScript/index.js
@@ -1,9 +1,20 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-function delayedCall(fn) {
-    setTimeout(fn, 2000);
+class Manager {
+    name;
+    age;
+    //you can add extra fields here
+    department;
+    constructor(name, age, department) {
+        this.name = name;
+        this.age = age;
+        this.department = department;
+    }
+    isLegal() {
+        return this.age > 18;
+    }
 }
-delayedCall(() => {
-    console.log('Hello after 1 second');
-});
+let person = new Manager("Sahil", 20, "accounts");
+console.log(person.name);
+console.log(person.isLegal());
 //# sourceMappingURL=index.js.map
--- TypeScript/index.ts
+++ TypeScript/index.ts
@@ -1,29 +1,38 @@
-//object as input
-interface Address{
-    city: String,
-    pin: number
-}
-interface User{
-    name:String,
-    age:number,
-    address?:Address,
-    isLegal: () => boolean,
-}
+// //object as input
+// interface Address{
+//     city: String,
+//     pin: number
+// }
+// interface User{
+//     name:String,
+//     age:number,
+//     address?:Address,
+//     isLegal: () => boolean,
+// }
+// class Manager implements User{
 
-class Manager implements User{
-    name: string;
-    age: number;
-    //you can add extra fields here
-    department: string;
-    constructor(name: string,age:number,department:string){
-        this.name= name;
-        this.age = age;
-        this.department = department;
-    } 
-    isLegal(){
-        return this.age>18;
+//     //you can add extra fields here, but the interface fiels are compulsory
+//     department: string;
+//     constructor(public name: string, public age:number, department:string){
+//         this.department = department;
+//     } 
+
+//     isLegal(){
+//         return this.age>18;
+//     }
+// }
+// let person = new Manager("Sahil",20,"accounts"); 
+// console.log(person.name);
+// console.log(person.isLegal());
+
+
+function getMax(nums: number[]): number{
+    let max: number = 0;
+    for(const num of nums){
+        if(num > max) max = num;
     }
+    return max;
 }
 
-let person = new Manager("Sahil",20,"accounts");
-console.log(person.name);
+const max = getMax([10, 20, 40, 60]);
+console.log(max);
