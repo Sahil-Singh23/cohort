fc5190ff4d7c98cafc37e7f9e5985686dd737e28
Sahil-Singh23 <sahil091205@gmail.com>
2025-12-29 18:59:34 +0530

    readonly, partial, pick, records and maps in ts

--- TypeScript/src/index.ts
+++ TypeScript/src/index.ts
@@ -1,21 +1,33 @@
-interface User{
-    email: string,
+// interface User{
+//     email: string,
+//     name: string,
+//     age: number,
+//     id: number,
+//     password: string
+// }
+
+// type updateProps = Pick<User,'name'|'age'|'email'>
+
+// type updatedPropsOptional = Partial<updateProps>;
+ 
+// function updateUser(updatedProps: updatedPropsOptional ){
+//     //hit the database to update the user    
+// }
+
+// function sumOfage(user1: User, user2: User):number{
+//     return user1.age+user2.age;
+// }
+
+// // console.log(sumOfage({name:"sahil",age:20},{name:"mike",age:35}));  
+
+
+type User={
     name: string,
     age: number,
-    id: number,
-    password: string
 }
 
-type updateProps = Pick<User,'name'|'age'|'email'>
-
-type updatedPropsOptional = Partial<updateProps>;
- 
-function updateUser(updatedProps: updatedPropsOptional ){
-    //hit the database to update the user    
+const usr: Readonly<User>={
+    name: 'John',
+    age: 21,
 }
-
-function sumOfage(user1: User, user2: User):number{
-    return user1.age+user2.age;
-}
-
-// console.log(sumOfage({name:"sahil",age:20},{name:"mike",age:35}));  
+usr.age= 14; // fails as age is read only 
