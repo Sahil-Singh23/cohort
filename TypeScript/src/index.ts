543e86a820c0a3af247c459884c6f5e9f1256b46
Sahil-Singh23 <sahil091205@gmail.com>
2025-12-29 18:34:23 +0530

    starting express with typescript

---
+++ TypeScript/src/index.ts
@@ -0,0 +1,21 @@
+interface User{
+    email: string,
+    name: string,
+    age: number,
+    id: number,
+    password: string
+}
+
+type updateProps = Pick<User,'name'|'age'|'email'>
+
+type updatedPropsOptional = Partial<updateProps>;
+ 
+function updateUser(updatedProps: updatedPropsOptional ){
+    //hit the database to update the user    
+}
+
+function sumOfage(user1: User, user2: User):number{
+    return user1.age+user2.age;
+}
+
+// console.log(sumOfage({name:"sahil",age:20},{name:"mike",age:35}));  
