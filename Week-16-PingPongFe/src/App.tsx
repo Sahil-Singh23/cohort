05226bfc2da9d9a1dbffa1c4c77e97dc6d47dd15
Sahil-Singh23 <sahil091205@gmail.com>
2026-01-08 14:05:20 +0530

    pingPong websocket project

--- Week-16-PingPongFe/src/App.tsx
+++ Week-16-PingPongFe/src/App.tsx
@@ -1,11 +1,38 @@
+import { useEffect, useRef } from 'react'
 import './App.css'
 
 function App() {
+  const socket = useRef<WebSocket | null>(null);
+  const inputRef = useRef<HTMLInputElement |null>(null);
 
+  useEffect(()=>{
+      socket.current = new WebSocket("ws://localhost:8000/");
+      console.log("control reached here")
+      socket.current.onopen = ()=> console.log("connected");
+
+      socket.current.onmessage = (e)=>{
+        //console.log(e);
+        alert(e.data);
+      }
+
+      return()=>{
+        socket.current?.close()
+      }
+  },[])
+
+  function sendMessage(){
+    if(!socket.current) return;
+    if (socket.current.readyState !== WebSocket.OPEN) return;
+    if(!inputRef.current) return;
+    socket.current.send(inputRef.current.value)
+    inputRef.current.value ="";
+  }
 
   return (
     <div>
-      
+      <input ref = {inputRef} type="text" placeholder='Enter text'></input>
+      <button onClick={sendMessage}>Send </button>
+    
     </div>
   )
 }
