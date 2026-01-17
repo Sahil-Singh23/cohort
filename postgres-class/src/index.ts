bba103d2a996a759a003ef310ee0be5c13d61614
Sahil-Singh23 <sahil091205@gmail.com>
2026-01-18 01:26:39 +0530

    learning how to use transactions in postgress

--- postgres-class/src/index.ts
+++ postgres-class/src/index.ts
@@ -1,7 +1,7 @@
 import {Client} from "pg";
-import express from "express"
+import express, { response } from "express"
 const app = express();
-app.use(express.json);
+app.use(express.json());
 
 const pgClient = new Client("postgresql://postgres:mysecretpassword@localhost:5432/postgres?sslmode=disable"); 
 
@@ -15,23 +15,40 @@ async function create(){
 create()
 
 app.post("/signup",async(req,res)=>{
-    const [username,password,email,city,country,street,pinCode] = req.body;
+    const {username,password,email,city,country,street,pinCode} = req.body;
 
     try{
-        pgClient.query("BEGIN")
-        const response = await pgClient.query("Insert into users(username,email,password) values($1,$2,$3) RETURNING id ",[username,password,email]); 
+        await pgClient.query("BEGIN")
+        const response = await pgClient.query("Insert into users(username,email,password) values($1,$2,$3) RETURNING id ",[username,email,password]); 
 
         const userId = response.rows[0].id;
 
         const addressRes = await pgClient.query("Insert into addresses (user_id,city,country,street,pincode) values($1,$2,$3,$4,$5) ",[userId,city,country,street,pinCode]); 
         
-        pgClient.query("COMMIT")
+        await pgClient.query("COMMIT")
 
         return res.json({message:"Sign up sucessfull "})
         
     }catch(e){
+        await pgClient.query("ROLLBACK")
         console.log(e);
         return res.status(401).json({message:"Error while signup "})
+    }
+})
+
+app.get("/metadata", async(req,res)=>{
+    const id = req.query.id;
+
+
+    //do not expose your password
+    const query = "SELECT users.id,username,email,user_id,city,country,street,pincode from users JOIN addresses on users.id=addresses.user_id where users.id = $1";
+
+    try{
+        const response = await pgClient.query(query,[id]);
+        return res.json({response:response.rows[0]})
+    }catch(e){
+        console.log(e);
+        return res.status(401).json({message:"Error getting data "})
     }
 })
 
