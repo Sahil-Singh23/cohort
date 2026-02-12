ba0521ebfe9f5cacbfe8a442af2d6b5b78889585
Sahil-Singh23 <sahil091205@gmail.com>
2026-02-12 12:14:34 +0530

    added dynamic routing

--- Next_app2/next_app2/app/blogs/[postID]/page.tsx
+++ Next_app2/next_app2/app/blogs/[postID]/page.tsx
@@ -3,10 +3,14 @@
 export default async function BlogPostPage({params}:{params:Promise<{postID:string}>}){
     const resolvedParams = await params;
     const postId = resolvedParams.postID;
+    const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`);
+    const data = await response.json();
 
     return(
         <div>
             Your on post {postId}
+            <h1>Post title: {data.title}</h1>
+            <h1>Post data: {data.body}</h1>
         </div>
     )
 }
