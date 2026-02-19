54323c43d46ab4e027a4e19f30282fbef3f81870
Sahil-Singh23 <sahil091205@gmail.com>
2026-02-19 12:03:50 +0530

    learning credentials provider in next auth

--- nextAuth_next/next_auth/app/api/auth/[...nextauth]/route.ts
+++ nextAuth_next/next_auth/app/api/auth/[...nextauth]/route.ts
@@ -1,9 +1,37 @@
-import { NextRequest, NextResponse } from "next/server";
+import CredentialsProvider from "next-auth/providers/credentials"
+import NextAuth from "next-auth"
 
-export function GET(request: NextRequest, { params }: { params: { routes?: string[] } }){
-    
-    return NextResponse.json({
-        message: "hello",
-        routes: params.routes || []
-    })
-}
+const handler = NextAuth({
+  providers: [
+  CredentialsProvider({
+    name: 'Login with email',
+    credentials: {
+      username: { label: "Username", type: "text", placeholder: "sahil121" },
+      password: { label: "Password", type: "password" }
+    },
+    async authorize(credentials, req) {
+      // You need to provide your own logic here that takes the credentials
+      // submitted and returns either a object representing a user or value
+      // that is false/null if the credentials are invalid.
+      // e.g. return { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
+      // You can also use the `req` object to obtain additional parameters
+      // (i.e., the request IP address)
+      const res = await fetch("/your/endpoint", {
+        method: 'POST',
+        body: JSON.stringify(credentials),
+        headers: { "Content-Type": "application/json" }
+      })
+      const user = await res.json()
+
+      // If no error and we have user data, return it
+      if (res.ok && user) {
+        return user
+      }
+      // Return null if user data could not be retrieved
+      return null
+    }
+  })
+]
+})
+
+export { handler as GET, handler as POST }
