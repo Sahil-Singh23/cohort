f36437b12c1efb0874b8c6a48ffedb7eb9c439a0
Sahil-Singh23 <sahil091205@gmail.com>
2026-02-19 15:56:47 +0530

    learning credentials provider in next auth

--- nextAuth_next/next_auth/app/page.tsx
+++ nextAuth_next/next_auth/app/page.tsx
@@ -1,13 +1,24 @@
-import { useSession } from "next-auth/react";
+"use client"
+import { SessionProvider, signIn, signOut, useSession } from "next-auth/react";
 import Image from "next/image";
 
 export default function Home() {
-   const session = useSession();
-  return (
    
+  return (
+   <SessionProvider>
+   
+    <RealHome></RealHome>
+    </SessionProvider>
+  );
+}
+
+function RealHome(){
+  const session = useSession(); 
+  return(
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      {session.status === "authenticated"? "Logout":"SignIn"}
+      {session.status === "authenticated" && <button onClick={()=>signOut()}>Log Out</button>}
+      {session.status === "unauthenticated" && <button onClick={()=>signIn()}>Sign In</button>}
       
     </div>
-  );
+  )
 }
